4377dbbebfe51a6532e86231b27ca6d90a1b1ab9
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-07 17:52:04 +0400

    add the structure for the Introduction.tsx file

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -8,7 +8,35 @@ const Introduction: React.FC = () => {
         <img src={IntroductionImg} alt="Introduction image" />
         <img src={RountAd} alt="Ad" />
       </div>
-      <div></div>
+      <div>
+        <div>
+          <h1>Discover Your Dream Property with Estatein</h1>
+          <p>
+            Your journey to finding the perfect property begins here. Explore
+            our listings to find the home that matches your dreams.
+          </p>
+        </div>
+        <div>
+          <button>Learn More</button>
+          <button>Browse Properties</button>
+        </div>
+        <section>
+          <div>
+            <div>
+              <p>200+</p>
+              <span>Happy Customers</span>
+            </div>
+            <div>
+              <p>10+</p>
+              <span>Properties For Clients</span>
+            </div>
+          </div>
+          <div>
+            <p>16+</p>
+            <span>Years of Experience</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
