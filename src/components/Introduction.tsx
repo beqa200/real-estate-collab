dd4ec2d986bb8973dcbfab8c8f7308aac6362d31
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-07 17:44:26 +0400

    add introduction pictures

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,10 +1,12 @@
-import IntroductionImg from "../images/introduction.jpg";
+import IntroductionImg from "../images/introduction/introduction.png";
+import RountAd from "../images/Introduction/round-ad.png";
 
 const Introduction: React.FC = () => {
   return (
     <div>
       <div>
         <img src={IntroductionImg} alt="Introduction image" />
+        <img src={RountAd} alt="Ad" />
       </div>
       <div></div>
     </div>
