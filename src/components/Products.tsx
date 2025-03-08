9eb437806c59c7f68bba9e50ee398617b9542c1f
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-08 21:38:46 +0400

    add the scroll

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -5,6 +5,10 @@ import BuildingImg from "../images/home/building.png";
 import Stars from "../images/home/stars.png";
 import LeftArrow from "../images/home/arrow-left.png";
 import RightArrow from "../images/home/arrow-right.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+
+import "swiper/swiper-bundle.css";
 
 const Products: React.FC = () => {
   return (
@@ -16,42 +20,125 @@ const Products: React.FC = () => {
         offers a glimpse into exceptional homes and investments available
         through Estatein.
       </p>
-      <div className="mt-[4rem] p-[2.4rem] rounded-[1.2rem] bg-[#141414] border-1 border-[#262626]">
-        <img src={TemporaryImg} alt="Home" />
-        <h5 className="title text-[1.8rem] mt-[1.6rem]">
-          Rustic Retreat Cottage
-        </h5>
-        <p className="about mt-[0.2rem]">
-          An elegant 3-bedroom, 2.5-bathroom townhouse in a gated community...
-          <span className="text-white underline">Read More</span>
-        </p>
-        <div className="flex gap-[0.6rem] flex-wrap mt-[2rem]">
-          <div className="feature-container">
-            <img src={BedroomImg} alt="Bedroom" />
-            <span className="feature-text">4-Bedroom</span>
-          </div>
-          <div className="feature-container">
-            <img src={BathroomImg} alt="Bathroom" />
-            <span className="feature-text">3-Bathroom</span>
-          </div>
-          <div className="feature-container">
-            <img src={BuildingImg} alt="Building" />
-            <span className="feature-text">Villa</span>
-          </div>
-        </div>
-        <div className="flex justify-between mt-[2rem]">
-          <div>
-            <span className="about">Price</span>
-            <p className="title text-[1.8rem]">$550.000</p>
-          </div>
-          <button
-            className="w-[20.2rem] h-[4.9rem] rounded-[8px] bg-[#703bf7] 
+      <Swiper slidesPerView={1} spaceBetween={50}>
+        <SwiperSlide>
+          <div className="mt-[4rem] p-[2.4rem] rounded-[1.2rem] bg-[#141414] border-1 border-[#262626]">
+            <img src={TemporaryImg} alt="Home" />
+            <h5 className="title text-[1.8rem] mt-[1.6rem]">
+              Rustic Retreat Cottage
+            </h5>
+            <p className="about mt-[0.2rem]">
+              An elegant 3-bedroom, 2.5-bathroom townhouse in a gated
+              community...
+              <span className="text-white underline">Read More</span>
+            </p>
+            <div className="flex gap-[0.6rem] flex-wrap mt-[2rem]">
+              <div className="feature-container">
+                <img src={BedroomImg} alt="Bedroom" />
+                <span className="feature-text">4-Bedroom</span>
+              </div>
+              <div className="feature-container">
+                <img src={BathroomImg} alt="Bathroom" />
+                <span className="feature-text">3-Bathroom</span>
+              </div>
+              <div className="feature-container">
+                <img src={BuildingImg} alt="Building" />
+                <span className="feature-text">Villa</span>
+              </div>
+            </div>
+            <div className="flex justify-between mt-[2rem]">
+              <div>
+                <span className="about">Price</span>
+                <p className="title text-[1.8rem]">$550.000</p>
+              </div>
+              <button
+                className="w-[20.2rem] h-[4.9rem] rounded-[8px] bg-[#703bf7] 
                       text-[1.4rem] font-medium text-white"
-          >
-            View Property Details
-          </button>
-        </div>
-      </div>
+              >
+                View Property Details
+              </button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mt-[4rem] p-[2.4rem] rounded-[1.2rem] bg-[#141414] border-1 border-[#262626]">
+            <img src={TemporaryImg} alt="Home" />
+            <h5 className="title text-[1.8rem] mt-[1.6rem]">
+              Rustic Retreat Cottage
+            </h5>
+            <p className="about mt-[0.2rem]">
+              An elegant 3-bedroom, 2.5-bathroom townhouse in a gated
+              community...
+              <span className="text-white underline">Read More</span>
+            </p>
+            <div className="flex gap-[0.6rem] flex-wrap mt-[2rem]">
+              <div className="feature-container">
+                <img src={BedroomImg} alt="Bedroom" />
+                <span className="feature-text">4-Bedroom</span>
+              </div>
+              <div className="feature-container">
+                <img src={BathroomImg} alt="Bathroom" />
+                <span className="feature-text">3-Bathroom</span>
+              </div>
+              <div className="feature-container">
+                <img src={BuildingImg} alt="Building" />
+                <span className="feature-text">Villa</span>
+              </div>
+            </div>
+            <div className="flex justify-between mt-[2rem]">
+              <div>
+                <span className="about">Price</span>
+                <p className="title text-[1.8rem]">$550.000</p>
+              </div>
+              <button
+                className="w-[20.2rem] h-[4.9rem] rounded-[8px] bg-[#703bf7] 
+                      text-[1.4rem] font-medium text-white"
+              >
+                View Property Details
+              </button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mt-[4rem] p-[2.4rem] rounded-[1.2rem] bg-[#141414] border-1 border-[#262626]">
+            <img src={TemporaryImg} alt="Home" />
+            <h5 className="title text-[1.8rem] mt-[1.6rem]">
+              Rustic Retreat Cottage
+            </h5>
+            <p className="about mt-[0.2rem]">
+              An elegant 3-bedroom, 2.5-bathroom townhouse in a gated
+              community...
+              <span className="text-white underline">Read More</span>
+            </p>
+            <div className="flex gap-[0.6rem] flex-wrap mt-[2rem]">
+              <div className="feature-container">
+                <img src={BedroomImg} alt="Bedroom" />
+                <span className="feature-text">4-Bedroom</span>
+              </div>
+              <div className="feature-container">
+                <img src={BathroomImg} alt="Bathroom" />
+                <span className="feature-text">3-Bathroom</span>
+              </div>
+              <div className="feature-container">
+                <img src={BuildingImg} alt="Building" />
+                <span className="feature-text">Villa</span>
+              </div>
+            </div>
+            <div className="flex justify-between mt-[2rem]">
+              <div>
+                <span className="about">Price</span>
+                <p className="title text-[1.8rem]">$550.000</p>
+              </div>
+              <button
+                className="w-[20.2rem] h-[4.9rem] rounded-[8px] bg-[#703bf7] 
+                      text-[1.4rem] font-medium text-white"
+              >
+                View Property Details
+              </button>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
       <div className="flex justify-between border-t-1 border-t-[#262626] py-[1.6rem] mt-[3rem]">
         <button
           className="w-[15.5rem] py-[1.4rem] rounded-[8px] bg-[#1a1a1a] border-1 border-[#262626] 
