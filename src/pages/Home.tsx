e774dd03ff90ea9ee85883ccd29a7e7cffb4f9a6
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-08 16:20:44 +0400

    add e=components in Home.ts file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Introduction from "../components/Introduction";
 import Opportunities from "../components/Opportunities";
+import Homes from "../components/Homes";
 
 const Home: React.FC = () => {
   return (
     <>
       <Introduction />
       <Opportunities />
+      <Homes />
     </>
   );
 };
