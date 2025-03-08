c300947a219106011c19bf898f659ac4b24c081c
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-08 14:32:38 +0400

    add images for Opportunities.tsx

--- src/components/Opportunities.tsx
+++ src/components/Opportunities.tsx
@@ -1,5 +1,18 @@
+import FirstOpportunityIcon from "../images/home/opportunity-1.png";
+import SecondOpportunityIcon from "../images/home/opportunity-2.png";
+import ThirdOpportunityIcon from "../images/home/opportunity-3.png";
+import FourthOpportunityIcon from "../images/home/opportunity-4.png";
+import Arrow from "../images/home/arrow.png";
+
 const Opportunities: React.FC = () => {
-  return <></>;
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Opportunities;
