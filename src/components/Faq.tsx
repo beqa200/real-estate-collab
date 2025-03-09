213df82d5e9b7093c30dce9591b26697a6c80500
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-09 12:32:32 +0400

    add the half part of the Faq.tsx

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,12 +1,30 @@
 const Faq: React.FC<{ stars: string }> = ({ stars }) => {
   return (
-    <div className="w-[95.5%] mx-auto mt-[7.1rem]">
+    <div className="w-[95.5%] mx-auto mt-[6.1rem]">
       <img src={stars} alt="Stars" />
-      <h3 className="title">What Our Clients Says</h3>
+      <h3 className="title">Frequently Asked Questions</h3>
       <p className="about mt-[0.6rem]">
-        Read the success stories and heartfelt testimonials from our valued
-        clients. Discover why they chose Estatein for their real estate needs.
+        Find answers to common questions about Estatein's services, property
+        listings, and the real estate process. We're here to provide clarity and
+        assist you every step of the way.
       </p>
+      <div className="mt-[4rem] p-[3rem] rounded-[1rem] bg-[#141414] border-1 border-[#262626]">
+        <div>
+          <h6 className="title text-[1.8rem] mt-[2.4rem]">
+            How do I search for properties on Estatein?
+          </h6>
+          <p className="title text-[1.4rem] font-medium mt-[2rem] text-[#999]">
+            Learn how to use our user-friendly search tools to find properties
+            that match your criteria.
+          </p>
+          <button
+            className="w-full h-[4.6rem] rounded-[8px] bg-[#1a1a1a] border-1 border-[#262626]
+                      text-[1.4rem] font-medium leading-[1.31] teacking-[-0.08px] text-white mt-[2rem]"
+          >
+            Read More
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
