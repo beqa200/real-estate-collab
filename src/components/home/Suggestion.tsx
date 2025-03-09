ca93b1c9e151f1098c147039133ab44aa1de48e0
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-09 14:06:47 +0400

    Initialize Suggestion.tsx file, import it in Home.tsx. Also add the home components folder and move those home components in it

---
+++ src/components/home/Suggestion.tsx
@@ -0,0 +1,5 @@
+const Suggestion: React.FC = () => {
+  return <div></div>;
+};
+
+export default Suggestion;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import Introduction from "../components/Introduction";
-import Opportunities from "../components/Opportunities";
-import Products from "../components/Products";
-import Ratings from "../components/Ratings";
+import Introduction from "../components/home/Introduction";
+import Opportunities from "../components/home/Opportunities";
+import Products from "../components/home/Products";
+import Ratings from "../components/home/Ratings";
 import Stars from "../images/home/stars.png";
 import LeftArrow from "../images/home/arrow-left.png";
 import RightArrow from "../images/home/arrow-right.png";
-import Faq from "../components/faq";
+import Faq from "../components/home/Faq";
+import Suggestion from "../components/home/Suggestion";
 
 const Home: React.FC = () => {
   return (
@@ -15,6 +16,7 @@ const Home: React.FC = () => {
       <Products stars={Stars} leftArrow={LeftArrow} rightArrow={RightArrow} />
       <Ratings stars={Stars} leftArrow={LeftArrow} rightArrow={RightArrow} />
       <Faq stars={Stars} leftArrow={LeftArrow} rightArrow={RightArrow} />
+      <Suggestion />
     </>
   );
 };
