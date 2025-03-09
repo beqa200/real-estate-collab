9d28c85e809023229b77e51786283a65104ea624
seriojaa <kaliashvilisergi@gmail.com>
2025-03-09 14:48:20 +0400

    Complete Our Journey Section Responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,14 +2,36 @@ import aboutImage from '../images/home/about.png'
 export default function About() {
     return (
         <div>
-            <div className='mx-6'>
-                <img src={aboutImage} alt="" />
+            <div className=' lg:flex items-center max-w-[1400px] mx-auto lg:flex-row-reverse  '>
+                <div className='mx-6 bg-gray-900'>
+                    <img src={aboutImage} alt="" />
+                </div>
+                <div className='flex flex-col items-center'>
+                    <div className='flex lg:mt-5  gap-2 flex-col mx-6 items-start'>
+                        <h1 className='text-[white] lg:ml-24 text-[28px] font-bold'>Our Journey</h1>
+                        <p className='text-[#999999] lg:ml-24 max-w-[600px] text-[16px]'>Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary. Over the years, we've expanded our reach, forged valuable partnerships, and gained the trust of countless clients.</p>
+                    </div>
+                    <section className="flex flex-wrap gap-[1.2rem] mt-[4rem]">
+                        <div className="flex w-full gap-[1.2rem]">
+                            <div className="result-container">
+                                <p className="people-amount">200+</p>
+                                <span className="result">Happy Customers</span>
+                            </div>
+                            <div className="result-container">
+                                <p className="people-amount">10+</p>
+                                <span className="result">Properties For Clients</span>
+                            </div>
+                        </div>
+                        <div className="result-container">
+                            <p className="people-amount">16+</p>
+                            <span className="result">Years of Experience</span>
+                        </div>
+                    </section>
+                </div>
+
             </div>
-            <div className='flex flex-col mx-6 items-start'>
-                <h1 className='text-[white] text-[28px] font-bold'>Our Journey</h1>
-                <p className='text-[#999999] text-[16px]'>Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary. Over the years, we've expanded our reach, forged valuable partnerships, and gained the trust of countless clients.</p>
-            </div>
-            <section className="flex mx-6 flex-wrap gap-[1.2rem] mt-[4rem]">
+
+            {/* <section className="flex flex-wrap gap-[1.2rem] mt-[4rem]">
                 <div className="flex w-full gap-[1.2rem]">
                     <div className="result-container">
                         <p className="people-amount">200+</p>
@@ -24,7 +46,8 @@ export default function About() {
                     <p className="people-amount">16+</p>
                     <span className="result">Years of Experience</span>
                 </div>
-            </section>
+            </section> */}
         </div>
+
     )
 }
