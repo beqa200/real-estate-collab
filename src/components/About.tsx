d34e2b4ace3a7c7806abc65c62f1fcfae1688521
seriojaa <kaliashvilisergi@gmail.com>
2025-03-11 18:19:11 +0400

    Create About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+
+import Achievements from "./About/Achievements";
+import Experience from "./About/Experience";
+import OurJourney from "./About/OurJourney";
+
+import OurValues from './About/OurValues';
+export default function About() {
+    return (
+        <div>
+            <OurJourney />
+            <OurValues />
+            <Achievements />
+            <Experience />
+        </div>
+
+    )
+}
