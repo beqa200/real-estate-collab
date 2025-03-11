1958c54cb0bb23ca2298a1bd9b2350125b4da36f
seriojaa <kaliashvilisergi@gmail.com>
2025-03-11 13:03:42 +0400

    Install Axios To Fetch Info

--- src/components/About/Achievements.tsx
+++ src/components/About/Achievements.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+interface Achievement {
+    id: string,
+    title: string,
+    description: string
+}
 export default function Achievements() {
+    const [info, setInfo] = useState<Achievement[] | null>(null)
+    useEffect(() => {
+        const getInfo = () => {
+            axios.get('http://104.248.242.53:8000/about/achievements/')
+                .then((res) => {
+                    console.log(res)
+                    setInfo(res.data)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
+        getInfo()
+    }, [])
+
     return (
         <div>
-            achievements
+            <div>
+                <h1>Our Achievements</h1>
+                <p>Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary.</p>
+            </div>
+            <div className='text-white'>
+                {info ? (
+                    <ul>
+                        {info.map((achievement) => (
+                            <li key={achievement.id}>
+                                <h2>{achievement.title}</h2>
+                                <p>{achievement.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
         </div>
     )
 }
