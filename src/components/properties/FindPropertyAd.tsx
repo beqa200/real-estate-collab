a5d795c48060f1552dd5e7b8033aa15b4d000ef1
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-23 15:45:09 +0400

    add the tablet styls for the upper part of the properties page

--- src/components/properties/FindPropertyAd.tsx
+++ src/components/properties/FindPropertyAd.tsx
@@ -2,11 +2,20 @@ import React from "react";
 
 const FindPropertyAd: React.FC = () => {
   return (
-    <div className="custom-prop-container bg-custom-gradient mx-auto max-w-[50rem]">
-      <h4 className="text-[2.8rem] font-semibold leading-[1.5] text-white">
+    <div
+      className="custom-prop-container bg-custom-gradient mx-auto max-w-[50rem]
+                tablet:text-[3.8rem] tablet:max-w-[280rem] tablet:pt-[10rem] tablet:pr-[20rem] tablet:pb-[13rem] pl-[8rem]"
+    >
+      <h4
+        className="text-[2.8rem] font-semibold leading-[1.5] text-white
+                  tablet:text-[3.8rem]"
+      >
         Find Your Dream Property
       </h4>
-      <p className="text-[1.4rem] font-medium leading-[1.5] text-[#999] mt-[1rem]">
+      <p
+        className="text-[1.4rem] font-medium leading-[1.5] text-[#999] mt-[1rem]
+                  tablet:text-[1.6rem]"
+      >
         Welcome to Estatein, where your dream property awaits in every corner of
         our beautiful world. Explore our curated selection of properties, each
         offering a unique story and a chance to redefine your life. With
