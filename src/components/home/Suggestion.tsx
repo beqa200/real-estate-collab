13bd310fe0b107eb65904caf45cdc5b05178b18b
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-03-24 23:49:50 +0400

    add the styles for the desktop version in Suggestion.tsx file

--- src/components/home/Suggestion.tsx
+++ src/components/home/Suggestion.tsx
@@ -5,13 +5,14 @@ const Suggestion: React.FC = () => {
   return (
     <div
       className="w-full py-[5rem] px-[1.6rem] border-y-1 border-y-[#262626] mt-[6.4rem] relative overflow-hidden
-                tablet:flex tablet:items-center tablet:justify-between tablet:py-[6rem] tablet:px-[8rem]"
+                tablet:flex tablet:items-center tablet:justify-between tablet:py-[6rem] tablet:px-[8rem]
+                desktop:py-[10rem] desktop:px-[16.2rem]"
     >
       <div className="z-1 sticky">
-        <h3 className="title tablet:text-[3.8rem]">
+        <h3 className="title tablet:text-[3.8rem] desktop:text-[4.8rem]">
           Start Your Real Estate Journey Today
         </h3>
-        <p className="about mt-[0.6rem] tablet:mt-[1rem] tablet:w-[70%]">
+        <p className="about mt-[0.6rem] tablet:mt-[1rem] tablet:w-[70%] desktop: desktop:mt-[1.4rem]">
           Your dream property is just a click away. Whether you're looking for a
           new home, a strategic investment, or expert real estate advice,
           Estatein is here to assist you every step of the way. Take the first
@@ -22,7 +23,7 @@ const Suggestion: React.FC = () => {
       <button
         className="w-full h-[4.9rem] rounded-[8px] bg-[#703bf7]
                   text-[1.4rem] font-medium leading-[1.5] text-white mt-[3rem] z-1 sticky
-                  tablet:w-[25%]"
+                  tablet:w-[25%] desktop:w-[30%] desktop:h-[6.3rem] desktop:text-[1.8rem]"
       >
         Explore Properties
       </button>
