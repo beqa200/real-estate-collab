24d19880fadd739d6eb9c7db496bfb3af972dec5
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-04 23:55:46 +0400

    add the styles for GetInTouch.tsx file

--- src/components/contact/GetInTouch.tsx
+++ src/components/contact/GetInTouch.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const GetInTouch: React.FC = () => {
   return (
-    <div>
-      <h1>Get in Touch with Estatein</h1>
-      <p>
+    <div className="py-[5rem] px-[1.6rem] bg-get-in-tourch-gradient border-b-1 border-[#262626]">
+      <h1 className="title">Get in Touch with Estatein</h1>
+      <p className="about mt-[1rem]">
         Welcome to Estatein's Contact Us page. We're here to assist you with any
         inquiries, requests, or feedback you may have.
       </p>
