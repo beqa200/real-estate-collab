dd2224a2ac1035029c7b2ae77fbc81125bbdfe93
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-04 23:47:13 +0400

    add the structure of GetInTouch.tsx file

--- src/components/contact/GetInTouch.tsx
+++ src/components/contact/GetInTouch.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const GetInTouch: React.FC = () => {
-  return <></>;
+  return (
+    <div>
+      <h1>Get in Touch with Estatein</h1>
+      <p>
+        Welcome to Estatein's Contact Us page. We're here to assist you with any
+        inquiries, requests, or feedback you may have.
+      </p>
+    </div>
+  );
 };
 
 export default GetInTouch;
