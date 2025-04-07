77c284ca69ab23c5ac6da62bf6614b3e024f1cf9
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-07 16:46:49 +0400

    download and import images

--- src/components/contact/Explore.tsx
+++ src/components/contact/Explore.tsx
@@ -1,3 +1,10 @@
+import ExploreImg1 from "../../images/contact/explore-img1.png";
+import ExploreImg2 from "../../images/contact/explore-img2.png";
+import ExploreImg3 from "../../images/contact/explore-img3.png";
+import ExploreImg4 from "../../images/contact/explore-img4.png";
+import ExploreImg5 from "../../images/contact/explore-img5.png";
+import AbstractVisual from "../../images/contact/abstract-background.png";
+
 const Explore: React.FC = () => {
   return <div>Explore</div>;
 };
