7f0b20ed27c7aac75f41be7983ba68f53c1632f6
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-09 19:36:11 +0400

    add the tablet styles for the office tour component

--- src/components/contact/OfficeTour.tsx
+++ src/components/contact/OfficeTour.tsx
@@ -4,31 +4,40 @@ import Email from "../../images/properties/mobile/email.png";
 import Location from "../../images/contact/icon-location-white.png";
 
 const OfficeTour: React.FC = () => {
-  const { Stars } = useGeneral();
+  const {
+    Stars,
+    isLargeScreen,
+    setIsLargeScreen,
+    isLargestScreen,
+    setIsLargestScreen,
+  } = useGeneral();
 
   return (
-    <div className="w-[91.5%] mt-[6.1rem] mx-auto max-w-[50rem]">
+    <div className="w-[91.5%] mt-[6.1rem] mx-auto max-w-[50rem] tablet:max-w-[280rem]">
       <img src={Stars} alt="Stars" className="custom-blend" />
-      <h3 className="title">Discover Our Office Locations</h3>
-      <p className="about mt-[0.8rem]">
-        Estatein is here to serve you across multiple locations. Whether you're
-        looking to meet our team.
+      <h3 className="contact-title">Discover Our Office Locations</h3>
+      <p className="about mt-[0.8rem] tablet:mt-[1rem]">
+        {!isLargeScreen && !isLargestScreen
+          ? "Estatein is here to serve you across multiple locations. Whether you'relooking to meet our team."
+          : isLargeScreen
+          ? "Estatein is here to serve you across multiple locations. Whether you're looking to meet our team, discuss real estate opportunities, or simply drop by for a chat, we have offices conveniently located to serve your needs. Explore the categories below to find the Estatein office nearest to you"
+          : ""}
       </p>
       <div
         className="flex items-center justify-center gap-[1rem] p-[1rem] rounded-[8px] 
-                  bg-[#1a1a1a] mt-[4rem]"
+                  bg-[#1a1a1a] mt-[4rem] tablet:w-[36%] tablet:mt-[6rem]"
       >
         <div className="office-info-filter">All</div>
         <div className="office-info-filter">Regional</div>
         <div className="office-info-filter">International</div>
       </div>
-      <div className="flex flex-col gap-[2rem] mt-[3rem]">
+      <div className="flex flex-col gap-[2rem] mt-[3rem] tablet:flex-row tablet:mt-[4rem]">
         <div className="office-type-container">
           <p className="office-type">Main Headquarters</p>
           <h5 className="office-location">
             123 Estatein Plaza, City Center, Metropolis
           </h5>
-          <p className="office-type text-[#999] mt-[0.8rem]">
+          <p className="office-type text-[#999] mt-[0.8rem] tablet:mt-[1rem]">
             Our main headquarters serve as the heart of Estatein. Located in the
             bustling city center, this is where our core team of experts
             operates, driving the excellence and innovation that define us.
@@ -55,10 +64,10 @@ const OfficeTour: React.FC = () => {
         </div>
         <div className="office-type-container">
           <p className="office-type">Regional Offices</p>
-          <h5 className="office-location">
+          <h5 className="office-location tablet:w-[103%]">
             456 Urban Avenue, Downtown District, Metropolis
           </h5>
-          <p className="office-type text-[#999] mt-[0.8rem]">
+          <p className="office-type text-[#999] mt-[0.8rem] tablet:mt-[1rem]">
             Estatein's presence extends to multiple regions, each with its own
             dynamic real estate landscape. Discover our regional offices,
             staffed by local experts who understand the nuances of their
