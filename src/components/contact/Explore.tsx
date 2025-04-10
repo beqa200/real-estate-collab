507219327a8e719e0acbc0d8281524cd174be6f4
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-04-10 15:05:53 +0400

    add the tablet styles for Explore.tsx file

--- src/components/contact/Explore.tsx
+++ src/components/contact/Explore.tsx
@@ -25,64 +25,84 @@ const Explore: React.FC = () => {
     <div
       className="w-[91.5%] mx-auto bg-[url('././images/contact/abstract-background.png')] bg-no-repeat py-[2rem] px-[1.45rem] 
                 bg-cover rounded-[12px] bg-[#1a1a1a] border-1 border-[#262626] mt-[8rem] max-w-[50rem]
-                tablet:max-w-[280rem] tablet:p-[6rem]"
+                tablet:max-w-[280rem] tablet:py-[4rem] tablet:bg-[url('././images/contact/tablet/abstract-background-medium.png')]
+                tablet:bg-top-left"
     >
       {!isLargeScreen && !isLargestScreen ? (
         <div className="grid grid-cols-2 gap-[1rem]">
           <img src={ExploreImg1} alt="Office" className="explore-img" />
           <img src={ExploreImg2} alt="Staff" className="explore-img" />
           <img src={ExploreImg3} alt="Working" className="explore-img" />
-          <div className="flex gap-[1rem]">
-            <img src={ExploreImg4} alt="Staff" className="h-full" />
-            <img src={ExploreImg5} alt="Staff" className="h-full" />
+          <div className="grid grid-cols-2 gap-[1rem]">
+            <img src={ExploreImg4} alt="Staff" className="explore-img" />
+            <img src={ExploreImg5} alt="Staff" className="explore-img" />
+          </div>
+          <div className="col-span-2">
+            <div className="bg-transparent">
+              <img
+                src={Stars}
+                alt="Stars"
+                className="bg-transparent custom-blend"
+              />
+            </div>
+            <div className="mt-[1.2rem]">
+              <h3 className="contact-title">Explore Estatein's World</h3>
+              <p className="about mt-[0.8rem] tablet:mt-[1rem]">
+                Step inside the world of Estatein, where professionalism meets
+                warmth, and expertise meets passion. Our gallery offers a
+                glimpse into our team and workspaces, inviting you to get to
+                know us better.
+              </p>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <div className="w-full object-contain">
+              <img
+                src={DealMaking}
+                alt="Deal making"
+                className="w-full object-contain"
+              />
+            </div>
           </div>
         </div>
       ) : isLargeScreen ? (
-        <div className="grid grid-cols-2 gap-[1rem]">
+        <div className="grid grid-cols-2 grid-flow-dense gap-[1.3rem] ">
           <img src={ExploreImg1Medium} alt="Office" className="explore-img" />
           <img src={ExploreImg2Medium} alt="Staff" className="explore-img" />
           <img src={ExploreImg3Medium} alt="Working" className="explore-img" />
-          <div className="flex gap-[1rem]">
-            <img src={ExploreImg4Medium} alt="Staff" className="h-full" />
-            <img src={ExploreImg5Medium} alt="Staff" className="h-full" />
+          <div className="grid grid-cols-2 gap-[1.3rem]">
+            <img src={ExploreImg4Medium} alt="Staff" className="explore-img" />
+            <img src={ExploreImg5Medium} alt="Staff" className="explore-img" />
+          </div>
+          <div>
+            <div className="bg-transparent">
+              <img
+                src={Stars}
+                alt="Stars"
+                className="bg-transparent custom-blend"
+              />
+            </div>
+            <div className="mt-[1.2rem]">
+              <h3 className="contact-title">Explore Estatein's World</h3>
+              <p className="about mt-[0.8rem] tablet:mt-[1rem]">
+                Step inside the world of Estatein, where professionalism meets
+                warmth, and expertise meets passion. Our gallery offers a
+                glimpse into our team and workspaces, inviting you to get to
+                know us better.
+              </p>
+            </div>
+          </div>
+          <div>
+            <div className="w-full object-contain">
+              <img
+                src={DealMakingMedium}
+                alt="Deal making"
+                className="w-full object-contain"
+              />
+            </div>
           </div>
         </div>
       ) : null}
-      <div className="bg-transparent">
-        <img
-          src={Stars}
-          alt="Stars"
-          className="mt-[2.5rem] bg-transparent custom-blend"
-        />
-      </div>
-
-      <div>
-        <div className="mt-[1.2rem]">
-          <h3 className="title">Explore Estatein's World</h3>
-          <p className="about mt-[0.8rem]">
-            Step inside the world of Estatein, where professionalism meets
-            warmth, and expertise meets passion. Our gallery offers a glimpse
-            into our team and workspaces, inviting you to get to know us better.
-          </p>
-        </div>
-        {!isLargeScreen && !isLargestScreen ? (
-          <div className="w-full object-contain mt-[2rem]">
-            <img
-              src={DealMaking}
-              alt="Deal making"
-              className="w-full object-contain"
-            />
-          </div>
-        ) : isLargeScreen ? (
-          <div className="w-full object-contain mt-[2rem]">
-            <img
-              src={DealMakingMedium}
-              alt="Deal making"
-              className="w-full object-contain"
-            />
-          </div>
-        ) : null}
-      </div>
     </div>
   );
 };
