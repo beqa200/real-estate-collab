7c07797a5a6d2fbdbada6ef36502f0153ff04b90
seriojaa <kaliashvilisergi@gmail.com>
2025-05-06 00:02:37 +0400

    Add new components, make with props to write less code

--- src/components/services/Unlock.tsx
+++ src/components/services/Unlock.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import FirstOpportunityIcon from "../../images/home/opportunity-1.png";
+import SecondOpportunityIcon from "../../images/home/opportunity-2.png";
+import ThirdOpportunityIcon from "../../images/home/opportunity-3.png";
+import Arrow from "../../images/home/arrow.png";
+import UnlockProps from './UnlockProps';
 export default function Unlock() {
     return (
-        <div className='mt-10'>
+        <div className='mt-10 '>
             <div className='flex flex-col gap-3'>
                 <h2 className='text-white font-bold text-6xl'>Unlock Property Value</h2>
                 <p className='text-[#999999] text-lg max-w-[800px]'>Selling your property should be a rewarding experience, and at Estatein, we make sure it is. Our Property Selling Service is designed to maximize <br /> the value of your property, ensuring you get the best deal possible. Explore the categories below to see how we can help you at every step of your <br /> selling journey</p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                <UnlockProps text={'Discover the true worth of your property with our expert valuation services.'} icon={FirstOpportunityIcon} arrow={'Valuation Mastery'} />
+                <UnlockProps text={'Selling a property requires more than just a listing; it demands a strategic marketing.'} icon={SecondOpportunityIcon} arrow={'Strategic Marketing'} />
+                <UnlockProps text={'Negotiating the best deal is an art, and our negotiation experts are masters of it.'} icon={ThirdOpportunityIcon} arrow={'Negotiation Wizardry'} />
             </div>
         </div>
     )
---
+++ src/components/services/UnlockProps.tsx
@@ -0,0 +1,26 @@
+interface BoxesProps {
+    text: string;
+    icon: string;
+    arrow: string;
+}
+
+export default function UnlockProps({ text, icon, arrow }: BoxesProps) {
+    return (
+        <div className="w-[380px] mx-auto mt-25 p-6 border border-[#262626] h-[160px] rounded-3xl">
+            <div className="flex justify-center mt-5 items-center gap-5 text-white">
+                <div className="w-20 h-20">
+                    <img src={icon} alt="icon" className="w-full h-full" />
+                </div>
+                <div className="text-white text-2xl">
+                    {arrow}
+                </div>
+            </div>
+
+            <div className="mt-4 text-center">
+                <p className="text-lg text-[#999999] max-w-[200px] mx-auto">{text}</p>
+            </div>
+        </div>
+
+
+    );
+}
