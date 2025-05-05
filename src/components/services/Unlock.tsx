fb7bddd7922ada337a919fb9621cec411254c50b
seriojaa <kaliashvilisergi@gmail.com>
2025-05-05 23:43:16 +0400

    created unlock component

---
+++ src/components/services/Unlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Unlock() {
+    return (
+        <div>
+            <div>
+                <h2 className='text-white font-bold text-6xl'>Unlock Property Value</h2>
+            </div>
+        </div>
+    )
+}
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,10 +1,12 @@
 import BoxesProps from '../components/services/BoxesProps';
 import Text from '../components/services/Text'
+import Unlock from '../components/services/Unlock';
 
 const Services: React.FC = () => {
   return <div className='max-w-[1200px]  mx-auto'>
     <Text />
     <BoxesProps />
+    <Unlock />
   </div>
 };
 
